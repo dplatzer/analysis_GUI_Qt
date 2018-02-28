10c3490d6ce6df84d3e244ccec85b46d24522f18
dplatzer <dominique.platzer@cea.fr>
2018-02-28 14:19:40 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 setup(name='analysis_GUI_Qt',
       version='1.0',
 	  description='The analysis_GUI_Qt Python library',
+	  long_description=open('README.md').read(),
 	  author='Dominique Platzer',
+	  url="https://github.com/dplatzer/analysis_GUI_Qt",
+	  install_requires=["pyqt>=5.6"],
 	  packages=find_packages(),
 	  include_package_data=True)
