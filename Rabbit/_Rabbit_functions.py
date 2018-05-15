- corrected an error when converting the attochirp from as per harmonic (2 omega) to as per eV. A factor 1/2 was missing - added a "clear" button in the rabbit tab and fixed a bug preventing to to a rabbit treatment several times (an (re)initialization of the variables was missing)

Rabbit/_Rabbit_functions.py
@@ -178,6 +178,12 @@ class Rabbit_functions_mixin:
 
     def normalrab_lr(self):
         ''' ANALYSIS - "Normal RABBIT" button listener'''
+        self.peak = []
+        self.peak_phase = []
+        self.fpeak = []
+        self.ampl = []
+        self.ang = []
+
         try:
             cts.rabbitmode = "normal"
             self.FT_ax.cla()
@@ -255,12 +261,13 @@ class Rabbit_functions_mixin:
             self.phase_ax.plot(sborder, attochirp, 'k')
             self.phase_fc.draw()
 
-            cts.chirp_as = self.pa[0]/(cts.cur_nu * 2*np.pi)/2 * 1e18
-            cts.chirp_as_eV = float(cts.chirp_as/(hnu))
+            cts.chirp_as = self.pa[0]/(2 * cts.cur_nu * 2*np.pi) * 1e18
+            cts.chirp_as_eV = float(cts.chirp_as/(2 * hnu))
 
             self.rainbowrab_btn.setEnabled(True)
             self.FTcontrast_btn.setEnabled(True)
             self.plotSBvsdelay_btn.setEnabled(True)
+            self.clear_btn.setEnabled(True)
 
             self.window().updateglobvar_fn()
         except:
@@ -351,3 +358,16 @@ class Rabbit_functions_mixin:
             rw = Rw.RainbowWin(self)
         except Exception:
             print(traceback.format_exception(*sys.exc_info()))
+
+    def clear_lr(self):
+        self.peak = []
+        self.peak_phase = []
+        self.fpeak = []
+        self.ampl = []
+        self.ang = []
+
+        self.phase_ax.cla()
+        self.FT_ax.cla()
+
+        self.phase_fc.draw()
+        self.FT_fc.draw()